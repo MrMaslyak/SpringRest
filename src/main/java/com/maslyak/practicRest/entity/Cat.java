f9f7974b8ce1d35e8910c848f87b2e855d7f059f
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-12-25 22:22:35 +0200

    add connect with PostgresSQL

--- src/main/java/com/maslyak/practicRest/entity/Cat.java
+++ src/main/java/com/maslyak/practicRest/entity/Cat.java
@@ -1,43 +1,33 @@
 package com.maslyak.practicRest.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "cats")
 public class Cat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
     private String name;
+
     private int age;
+
     private int weight;
 
-    // Конструктор с параметрами
     public Cat(String name, int age, int weight) {
         this.name = name;
         this.age = age;
         this.weight = weight;
     }
 
-    // Пустой конструктор (нужен для работы Jackson при десериализации)
     public Cat() {
     }
 
-    // Геттеры и сеттеры
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
 }
